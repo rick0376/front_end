9c839d4de93c978e0a8e72ad732e5ba08036b1b8
devrick <lhpsystems0376@gmail.com>
2025-03-04 08:16:36 -0300

    tste

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
-import axios from 'axios'
+export const runtime = 'nodejs';
 
+import axios from 'axios';
+
+// Criando a instância do axios
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API
-})
-
-
+  baseURL: process.env.NEXT_PUBLIC_API,
+});
