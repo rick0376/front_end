35328f5ae49ea7af9c878085761dccabb43659f5
devrick <lhpsystems0376@gmail.com>
2025-03-05 10:07:50 -0300

    t

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import { Orders } from "./components/orders";
 import { api } from '@/services/api'
 import { getCookieServer } from '@/lib/cookieServer'
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,3 @@
-export const runtime = 'nodejs';
-
 import axios from 'axios';
 
 // Criando a instância do axios
