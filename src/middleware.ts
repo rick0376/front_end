dc67791477506c04d0459f0531522201f3c6eba3
devrick <lhpsystems0376@gmail.com>
2025-03-05 12:29:24 -0300

    atualizando5

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,58 +1,41 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { getCookieServer } from '@/lib/cookieServer'
-import { api } from "@/services/api"
+import { NextRequest, NextResponse } from 'next/server';
+import { getCookieServer } from '@/lib/cookieServer';
+import { api } from '@/services/api';
 
-export async function middleware(req: NextRequest){
-  const { pathname } = req.nextUrl
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
 
-  if(pathname.startsWith("/_next") || pathname === "/"){
+  // Permite o acesso a rotas internas e à página inicial
+  if (pathname.startsWith('/_next') || pathname === '/') {
     return NextResponse.next();
   }
 
-  const token = getCookieServer();
-  
-  if(pathname.startsWith("/dashboard")){
-    if(!token){
-      return NextResponse.redirect(new URL("/", req.url))
+  const token: string | null = getCookieServer(); // Garantir que pode ser string ou null
+
+  if (pathname.startsWith('/dashboard')) {
+    if (!token) {
+      return NextResponse.redirect(new URL('/', req.url));
     }
 
-    const isValid = await validateToken(token)
+    const isValid: boolean = await validateToken(token);
     console.log(isValid);
 
-    if(!isValid){
-      return NextResponse.redirect(new URL("/", req.url))
+    if (!isValid) {
+      return NextResponse.redirect(new URL('/', req.url));
     }
   }
 
   return NextResponse.next();
-
 }
 
-/*
-async function validateToken(token: string){
-  if (!token) return false;
-
-  try{
-    await api.get("/me", {
-      headers:{
-        Authorization: `Bearer ${token}`
-      }
-    })
-
-    return true;
-  }catch(err){
-    return false;
-  }
-}*/
-
-async function validateToken(token: string): Promise<boolean> {
+async function validateToken(token: string | null): Promise<boolean> {
   if (!token) return false;
 
   try {
-    await api.get("/me", {
+    await api.get('/me', {
       headers: {
-        Authorization: `Bearer ${token}`
-      }
+        Authorization: `Bearer ${token}`,
+      },
     });
 
     return true;
